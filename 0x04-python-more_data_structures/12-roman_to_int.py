9bdf40e457bb003d962b15ed2986e4b75c251e7b
John Njuguna <johnmnjuguna89@gmail.com>
2023-08-17 23:27:47 -0700

    Converts roman numeral to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,6 @@
 roman = dict([('I', 1), ('V', 5), ('X', 10), ('L', 50), ('C', 100), ('D', 500),
     ('M', 1000), ('IV', 4), ('IX', 9), ('XL', 40),
     ('XC', 90), ('CD', 400), ('CM', 900)])
-
 def roman_list(rom_str):
     lists = []
     x = 0
@@ -14,7 +13,6 @@ def roman_list(rom_str):
             lists += [rom_str[x]]
             x += 1
     return lists
-
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
