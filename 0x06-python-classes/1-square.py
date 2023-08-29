891c5afdf074444c35eb95f8ffaee7763b62e57a
John Njuguna <johnmnjuguna89@gmail.com>
2023-08-29 08:40:22 -0700

    Got the size of my class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This is a Class Square """
+
+
+class Square:
+    """ Initializing my Square
+    Args:
+       __size: private instance atribute size of Square
+    """
+    def __init__(self, size):
+        self.__size = size
