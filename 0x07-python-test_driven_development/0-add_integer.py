91043cb0cff98e28a2e391c9259b0e32b5edac57
John Njuguna <johnmnjuguna89@gmail.com>
2023-09-04 01:39:49 -0700

    Adds two integer and test if the test pass or fail

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+   Calculation of Module
+"""
+
+
+def add_integer(a, b=98):
+    """
+       Add two integers
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
