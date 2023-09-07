c0b074cd47bca605fae4fd03f4e1026eb32c4dd7
John Njuguna <johnmnjuguna89@gmail.com>
2023-09-07 07:14:15 -0700

    Multiplies two matrixcs

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+   Matrix Multiplication With Numpy Module
+"""
+import numpy as npy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+       Multiplication of two matrices
+    """
+    return npy.matrix_mul(m_a, m_b)
