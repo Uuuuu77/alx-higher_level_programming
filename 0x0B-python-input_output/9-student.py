cd0bff5cc1a92c5461631f5142313e372e4237db
John Njuguna <johnmnjuguna89@gmail.com>
2023-09-12 13:30:48 -0700

    Class Student that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+   Module of class student
+"""
+
+
+class Student:
+    """
+       Class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+           Retrieves dictionary representation of Student instance
+        """
+        return self.__dict__
