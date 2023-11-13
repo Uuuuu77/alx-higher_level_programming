3d1a306dbf355104dac9219f0b0709a49abf1cbe
John Njuguna <johnmnjuguna89@gmail.com>
2023-11-13 08:41:58 -0800

    Prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const myVar1 = process.argv[2];
+const myVar2 = parseInt(myVar1, 10);
+if (myVar2) {
+  let square = '';
+  for (let x = 0; x < myVar2; x++) {
+    square += 'X';
+  }
+  for (let y = 0; y < myVar2; y++) {
+    console.log(square);
+  }
+} else {
+    console.log('Missing size');
+}
