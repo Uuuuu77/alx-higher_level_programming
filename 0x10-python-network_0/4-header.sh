116ee834381d4dbfca18f93052c5382afcd090ab
John Njuguna <johnmnjuguna89@gmail.com>
2023-11-30 06:49:01 -0800

    Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A bash script that takes in a URL as an argument,
-# sends a GET request to the URL, and displays the body of the response.
+# A bash script that takes in a URL as an argument and displays the body.
 curl -sH "X-School-User-Id: 98" "$1"
