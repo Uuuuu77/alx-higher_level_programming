989685bcca27b88ef7baa3bab1b6fbfb9c1cd02f
John Njuguna <johnmnjuguna89@gmail.com>
2023-11-30 06:56:57 -0800

    cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A bash script that ends a JSON POST request to a URL passed
-# as the first argument, and displays the body of the response.
+# A bash script that ends a JSON POST request to a URL passed and displays body
 curl -sX POST -H "Content-Type: application/json" -d @$2 "$1"
