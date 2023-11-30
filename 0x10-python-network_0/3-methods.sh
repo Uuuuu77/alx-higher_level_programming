4aa31a6e9a83f99addabc2507a74cc64e0773e01
John Njuguna <johnmnjuguna89@gmail.com>
2023-11-30 06:00:33 -0800

    Takes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A bash script that takes in a URL and displays 
 # all HTTP methods the server will accept.
-curl -SI "$1" | grep "Allow" | cut -d " " -f2-
+curl -SI "$1" | grep "Allow" | cut -c 8-
