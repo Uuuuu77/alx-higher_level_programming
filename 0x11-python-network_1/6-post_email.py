401c9cc7874970587dc3cff42a297233ae64be0c
John Njuguna <johnmnjuguna89@gmail.com>
2023-12-01 07:24:50 -0800

    POST an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     Module that akes in a URL and an email address, sends a POST request
-    to the passed URL with the email as a parameter, 
+    to the passed URL with the email as a parameter,
     and finally displays the body of the response.
 """
 from requests import post
