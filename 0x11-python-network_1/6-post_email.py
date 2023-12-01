5a91578fcc2c7f7f91fc223fffc5c2b30781b5d0
John Njuguna <johnmnjuguna89@gmail.com>
2023-12-01 07:22:43 -0800

    POST an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Module that akes in a URL and an email address, sends a POST request
+    to the passed URL with the email as a parameter, 
+    and finally displays the body of the response.
+"""
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+    content = post(argv[1], data={'email': argv[2]})
+    print(content.text)
