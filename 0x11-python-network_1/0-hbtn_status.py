2faef15c6a58d07007dd31fee95b875d40265256
John Njuguna <johnmnjuguna89@gmail.com>
2023-12-01 05:57:11 -0800

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Module that fetches status.
+"""
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        utf = content.decode('utf-8')
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {utf}")
